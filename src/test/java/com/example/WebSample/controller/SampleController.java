abf2cb36dfe0457315fbd2dc61035f799bfee70a
HeaSeongYoon <yoonhs989898@gmail.com>
2023-07-16 01:06:51 +0900

    예외처리 완료

--- src/test/java/com/example/WebSample/controller/SampleController.java
+++ src/test/java/com/example/WebSample/controller/SampleController.java
@@ -41,40 +41,6 @@ public class SampleController {
     }
 
 
-    @ExceptionHandler(IllegalAccessException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalAccessException(
-            IllegalAccessException e) {
-        log.error("IllegalAccessException is occurred.", e);
-
-        return ResponseEntity
-                .status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(ErrorCode.TOO_BIG_ID_ERROR,
-                        "IllegalAccessException is occurred."));
-    }
-
-    @ExceptionHandler(WebSampleException.class)
-    public ResponseEntity<ErrorResponse> handleWebSampleException(
-            WebSampleException e) {
-        log.error("WebSampleException is occurred.", e);
-
-        return ResponseEntity
-                .status(HttpStatus.INSUFFICIENT_STORAGE)
-                .body(new ErrorResponse(e.getErrorCode(),
-                        "IllegalAccessException is occurred."));
-    }
-
-
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(
-            Exception e) {
-        log.error("Exception is occurred.", e);
-
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(ErrorCode.INTERNAL_SEVER_ERROR,
-                        "IllegalAccessException is occurred."));
-    }
 
 
 
--- src/test/java/com/example/WebSample/exception/ErrorCode.java
+++ src/test/java/com/example/WebSample/exception/ErrorCode.java
@@ -4,6 +4,8 @@ package com.example.WebSample.exception;
 
 public enum ErrorCode {
     INTERNAL_SEVER_ERROR,
+    NEW_OLD_UNMATCH,
+    REFUND_AMOUNT_EXCEED_PAYMENT_AMOUNT,
     TOO_BIG_ID_ERROR,
     TOO_SMALL_ID_ERROR
 }
